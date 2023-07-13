8df909823a0bbae886f88c12f13f28a0f6b034b6
JonasBusto <jonasrex00@gmail.com>
2023-07-13 14:12:50 -0300

    env

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ const mysql2 = require("mysql2/promise");
 const createPool = mysql2.createPool;
 
 const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "12345678",
-  port: 3306,
-  database: "virtualizaciondb",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
 });
 
 module.exports = pool;
