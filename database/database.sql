c302162e5dba6d53f40fd4ff4d74adf3a3312b0b
Jonas Busto <jonasrex00@gmail.com>
2023-07-16 21:09:14 -0300

    backend v1.0

--- database/database.sql
+++ database/database.sql
@@ -1,28 +1,90 @@
-/* Pasos para crear, modificar, eliminar y leer la tabla herramienta */
+/* 
 
+La base de datos, realizada con MySQL//MariaDB se llama 'virtualizaciondb' 
+  
+  Consta de las siguientes tablas:
+
+  # blog (idBlog, descripcion);
+  # herramienta (id, nombre, info_extra, img_logo, descripcion, url)
+  # estudiante (idEstudiante, nombre, apellido, edad, email, presentacion, url_img_perfil)
+  # sobre_mi (idSobreMi, descripcion)
+
+ */
+
+
+/* LISTA DE COMANDOS PARA ARMAR LA DB Y LAS TABLAS: */
+
+
+/* Crear la DB si no existe */
 CREATE DATABASE IF NOT EXISTS virtualizaciondb;
 
+/* Acceder a la DB creada */
 USE virtualizaciondb;
 
-CREATE TABLE herramienta (
-  id INT(15) NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR(100) DEFAULT NULL,
-  info_extra VARCHAR(100) DEFAULT NULL,
-  img_logo VARCHAR(1000) DEFAULT NULL,
-  descripcion VARCHAR(5000) DEFAULT NULL,
-  url VARCHAR(1000) DEFAULT NULL,
-  PRIMARY KEY(id)
-);
+/* Crear las tablas para la DB creada */
+CREATE TABLE IF NOT EXISTS `virtualizaciondb`.`blog` (
+  `idBlog` INT NOT NULL AUTO_INCREMENT,
+  `descripcion` VARCHAR(5000) NULL,
+  `urlPDF` VARCHAR(5000) NULL,
+  PRIMARY KEY (`idBlog`)
+  )
+ENGINE = InnoDB;
 
-DESCRIBE herramienta;
+CREATE TABLE IF NOT EXISTS `virtualizaciondb`.`estudiante` (
+  `idEstudiante` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(200) NULL,
+  `apellido` VARCHAR(200) NULL,
+  `edad` INT NULL,
+  `email` VARCHAR(200) NULL,
+  `presentacion` VARCHAR(5000) NULL,
+  `url_img_perfil` VARCHAR(1000) NULL,
+  `Blog_idBlog` INT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`idEstudiante`, `Blog_idBlog`),
+  INDEX `fk_Estudiante_Blog1_idx` (`Blog_idBlog` ASC) VISIBLE,
+  CONSTRAINT `fk_Estudiante_Blog1`
+    FOREIGN KEY (`Blog_idBlog`)
+    REFERENCES `virtualizaciondb`.`blog` (`idBlog`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-INSERT INTO herramienta VALUES
-  (1, 'Proxmox', 'v8.0', 'https://static-00.iconduck.com/assets.00/proxmox-icon-512x512-wdyg8x2z.png','Proxmox es una plataforma de virtualización de código abierto que combina la virtualización basada en el hipervisor KVM y la virtualización a nivel de contenedor utilizando LXC. Proporciona una interfaz web intuitiva y funciones avanzadas como migración en vivo de máquinas virtuales, clústeres de alta disponibilidad y copias de seguridad integradas.','https://www.proxmox.com/en/');
+CREATE TABLE IF NOT EXISTS `virtualizaciondb`.`sobre_mi` (
+  `idSobreMi` INT NOT NULL AUTO_INCREMENT,
+  `descripcion` VARCHAR(1000) NULL,
+  `Estudiante_idEstudiante` INT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`idSobreMi`, `Estudiante_idEstudiante`),
+  INDEX `fk_Conocimiento_Estudiante_idx` (`Estudiante_idEstudiante` ASC) VISIBLE,
+  CONSTRAINT `fk_Conocimiento_Estudiante`
+    FOREIGN KEY (`Estudiante_idEstudiante`)
+    REFERENCES `virtualizaciondb`.`estudiante` (`idEstudiante`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-UPDATE herramienta SET nombre = IFNULL(?, nombre), info_extra = IFNULL(?, info_extra), img_logo = IFNULL(?, img_logo), descripcion = IFNULL(?, descripcion), url = IFNULL(?, url) WHERE id = 1;
+CREATE TABLE IF NOT EXISTS `virtualizaciondb`.`herramienta` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NULL,
+  `info_extra` VARCHAR(100) NULL,
+  `img_logo` VARCHAR(1000) NULL,
+  `descripcion` VARCHAR(5000) NULL,
+  `url` VARCHAR(1000) NULL,
+  `Blog_idBlog` INT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`, `Blog_idBlog`),
+  INDEX `fk_Herramienta_Blog1_idx` (`Blog_idBlog` ASC) VISIBLE,
+  CONSTRAINT `fk_Herramienta_Blog1`
+    FOREIGN KEY (`Blog_idBlog`)
+    REFERENCES `virtualizaciondb`.`blog` (`idBlog`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DELETE FROM herramienta WHERE id=1;
 
-DROP TABLE herramienta;
+INSERT INTO blog VALUES (1, 'El blog personal consta de una estructura básica definida en
+              secciones, que organizan la información necesaria y requerida por
+              la materia. Se empleo HTML, CSS, JavaScript y Google Firebase para la creación del
+              Frontend del blog, y NodeJs como MariaDB para el Backend, donde
+              ambas partes fueron subidas a los contenedores de Proxmox.', 'https://drive.google.com/file/d/1MPbZxkoG7aPbZyUqdCV8C_W_mLp1DnXI/preview');
 
- ALTER TABLE herramienta ALTER Blog_idBlog SET DEFAULT 1;
+INSERT INTO estudiante VALUES (1, 'Kevin Jonás', 'Busto', 23, 'Kevin.Busto@alu.frt.utn.edu.ar', 'Tengo 23 años. Estudio Ing. Sistemas...', 'https://drive.google.com/uc?export=view&id=1EemG9V23wiT_tado0MqbqQ8BoUY7vKFO', 1);
+
+
